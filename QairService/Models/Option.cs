dc95d6ea9cde8d3affe5e4f5a2fc8dfebb7e33a5
mohdkafi <mohdkafi34@gmail.com>
2023-03-06 01:30:22 +0400

    Add business clasess

---
+++ QairService/Models/Option.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace QairService.Models
+{
+    public abstract class Option<T>
+    {
+        protected T? Value;
+        public abstract void setValue(T value);
+        public abstract T getValue();
+    }
+}
---
+++ QairService/Models/OptionString.cs
@@ -0,0 +1,16 @@
+﻿namespace QairService.Models
+{
+    public class OptionString : Option<string>
+    {
+
+        public override string getValue()
+        {
+            return Value?? string.Empty;
+        }
+
+        public override void setValue(string value)
+        {
+            Value = value;
+        }
+    }
+}
